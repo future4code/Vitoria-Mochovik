c09bd828e1f77959510ab442f7f0b50a8a0939d2
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-03 16:42:52 -0300

    Exercício 03 de interpretação

--- semana1/variaveis/index.js
+++ semana1/variaveis/index.js
@@ -24,6 +24,17 @@ console.log(a, b, c)
 2. RESPOSTA
 10 10 10
 ---------------------------------------------
+3.
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert('Voce recebe ${t/p} por hora')
+
+3. RESPOSTA
+A variável "p" pode ser chamada de horasDeTrabalhoPorDia
+A variável "t" pode ser chamada de dinheiroRecebidoPorDia
+
+-----------------------------------------------
 
 */
 
