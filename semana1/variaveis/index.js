9f53bf57c59d87acc73d610aa0bdb4fbc0e0ee3b
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-03 16:39:59 -0300

    Exercício 01 de interpretação

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,17 @@
+/*
+- Exercícios de interpretação de código
+---------------------------------------------
+
+1. 
+let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a, b)
+
+1. RESPOSTA:
+10
+10 5
+---------------------------------------------
+
+*/
