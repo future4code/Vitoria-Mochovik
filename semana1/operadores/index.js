85350da0563f71d15964fe2f041a062d44a0eb70
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-04 13:59:18 -0300

    Exercício 02 de interpretação de código

--- semana1/operadores/index.js
+++ semana1/operadores/index.js
@@ -7,6 +7,13 @@
  true
  boolean
 
+ 2.RESPOSTA
+ Vai ser impresso o primeiro número que o usuário inseriu ao lado do segundo número.
+ Se o usuário inseriu 1 e 2:
+ O programa imprimirá: 12
  
+ Esse erro acontece pois quando passa pelo "prompt" 
+ o resultado sai como string, então precisa-se converter para número.
+
 */
 
