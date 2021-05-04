279313df596d0f1c99b879b2e7945f6abfd20596
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-04 14:42:00 -0300

    Exercício 04 de escrita de código

--- semana1/operadores/index.js
+++ semana1/operadores/index.js
@@ -69,4 +69,16 @@ console.log("Você tem", idadeHoras, "HORAS de idade")
 
 */
 
+// EXERCÍCIO 04-------------------------------------------------------
+
+const numero1 = Number(prompt("Digite o primeiro número"))
+const numero2 = Number(prompt("Digite o segundo número"))
+
+console.log("Você inseriu:", numero1, "e", numero2)
+console.log("O primeiro número é maior que o segundo?", numero1 > numero2)
+console.log("O primeiro número é igual ao segundo?", numero1 === numero2)
+console.log("O primeiro número é divisível pelo segundo?", (numero1 % numero2) === 0)
+console.log("O segundo número é divisível pelo primeiro?", (numero2 % numero1) === 0)
+
+
 
