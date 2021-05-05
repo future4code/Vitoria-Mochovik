223b12fd17293b8e76729059d9cb8cd35bea603a
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-05 12:04:09 -0300

    Exercício 01 de interpretação de código

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,19 @@
+//-----------------------Exercícios de interpretação de código-----------------------------
+
+//EXERCÍCIO 01
+
+/*
+1. RESPOSTA
+
+a. undefined
+b. null
+c. 11
+d. 3
+e. 
+Array(11) [ 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 ] 
+f. 9
+
+*/
+
+
+
