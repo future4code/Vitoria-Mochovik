e02aede35ec6e1ecbc9ba30494cadfc5fa26fe99
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-06 11:51:06 -0300

    Exercício 02 de interpretação de código

--- semana1/funcoes/index.js
+++ semana1/funcoes/index.js
@@ -14,3 +14,25 @@ Caso retirassemos os dois 'console.log', NADA aparecia no console,
 mas computador realizaria o cálculo pois a função foi invocada, se usássemos um debug
 veriamos que o computador realizou isso.
 */
+
+//EXERCÍCIO 02-------------------------------------------------------------------
+
+
+/*
+2A.
+
+A função 'outraFuncao' recebe um texto que o usuário escreveu por meio do prompt, esse texto foi coletado
+no programa principal e salvo na variável 'textoDoUsuario', ele então chega como parâmetro 'texto' até a função.
+A função então converte todo o texto do usuário para letras minúsculas, e então procura se existe a palavra 'cenoura'
+na frase por ele inserida, caso exista ele retorna 'true', do contrário, 'false'.
+
+Sua utilidade é identificar se existe determininada palavra (nesse caso 'cenoura') no texto do usuário, em um caso real 
+pode ser bem utilizada na construção de um chatboot onde o programa identifica o que o usuário deseja.
+
+2B.
+
+I - true
+II - true
+III - true
+
+*/
