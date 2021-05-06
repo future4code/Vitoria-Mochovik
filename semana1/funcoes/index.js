c0a301a29062a549de82df805c7d058cc97053f6
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-06 11:34:46 -0300

    Exercício 01 de interpretação de código

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,16 @@
+//---------------------Exercícios de interpretação de código---------------------
+
+//EXERCÍCIO 01-------------------------------------------------------------------
+
+/*
+1A.
+
+10
+50
+
+1B.
+
+Caso retirassemos os dois 'console.log', NADA aparecia no console,
+mas computador realizaria o cálculo pois a função foi invocada, se usássemos um debug
+veriamos que o computador realizou isso.
+*/
