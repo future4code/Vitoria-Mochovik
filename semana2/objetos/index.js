ad64c911ea2cc0b664f16237da6eb462425f84dd
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-10 12:36:03 -0300

    Exercício 02 de interpretação de código

--- semana2/objetos/index.js
+++ semana2/objetos/index.js
@@ -6,6 +6,18 @@
 //Virginia Cavendish
 //{ canal: 'Globo', horario: '14h' }
 
+//EXERCÍCIO 02--------------------------------------------------------------------
+
+/*
+2A
+Nada será impresso no console já que temos um erro em 'const tartaruga = ...',
+pois não consigo usar o método 'replaceAll()' dessa forma.
+
+2B
+Os três pontos fazem uma cópia completa do objeto mencionado para outro objeto,
+e isso nos permite mudar ou adicionar algumas propriedades referente ao objeto copiado.
+
+*/
 
 
 //------------------Exercício de escrita de código--------------------------------
