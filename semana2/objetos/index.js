739f72f24dc1a6dd2b148e42de25f5328668d5cc
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-10 12:21:06 -0300

    Exercício 01 de interpretação de código

---
+++ semana2/objetos/index.js
@@ -0,0 +1,11 @@
+//------------------Exercício de interpretação de código--------------------------
+
+//EXERCÍCIO 01--------------------------------------------------------------------
+
+//Matheus Nachtergaele
+//Virginia Cavendish
+//{ canal: 'Globo', horario: '14h' }
+
+
+
+//------------------Exercício de escrita de código--------------------------------
