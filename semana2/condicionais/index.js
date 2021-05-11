721436754dc20f47611b5efda1bd2c775b32bb25
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-11 11:42:31 -0300

    Exercício 01 de interpretação de código

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,21 @@
+//---------------------Exercícios de interpretação de código---------------------------------
+
+//EXERCÍCIO 01-------------------------------------------------------------------------------
+
+/*
+
+1A
+O programa solicita ao usuário um número, o código transforma essa entrada que esta em string
+em número, com o comando `Number()`. Feito isso o programa verifica se o valor inserido é um par, 
+ou seja, se o resto da divisão dele por 2 for igual a 0 significa que é par e então o programa 
+imprime no console: "Passou no teste", caso contrário, ele imprime "Não passou no teste".
+
+1B
+Ele imprime no console "Passou no teste" para todos os valores que passarem no teste de ter o 
+resto da divisão por 2 igual à zero, ou seja, todos os valores que são pares.
+
+1C
+Ele imprime no console "Não passou no teste" para todos os valores que não passarem no teste,
+todos que o resto da divisão por 2 for diferente de 0, ou seja, todos que forem ímpares.
+
+*/
