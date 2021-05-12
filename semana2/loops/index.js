c5c96f81e984a39156de6d10da007c3c4aded6aa
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-12 12:17:06 -0300

    Exercício 01 de interpretação de código

---
+++ semana2/loops/index.js
@@ -0,0 +1,43 @@
+//---------------------Exercício de interpretação de código------------------------------
+
+//EXERCÍCIO 01---------------------------------------------------------------------------
+
+/*
+
+Basicamente o código conta com uma variável do tipo let chamada `valor` que é atribuido o 
+valor 0. Então o programa entra em um laço de for que fica de i = 0 até i = 4, e cada vez 
+que passar por esse for será somado o valor de `i` ao valor da variável `valor`.
+->Primeira vez no for 
+i = 0
+valor = 0
+valor = valor + i
+valor = 0 + 0 = 0
+
+->Segunda vez no for
+i = 1
+valor = 0
+valor = valor + i
+valor = 0 + 1 = 1
+
+->Terceira vez no for
+i = 2
+valor = 1
+valor = valor + i
+valor = 1 + 2 = 3
+
+->Quarta vez no for
+i = 3
+valor = 3
+valor = valor + i
+valor = 3 + 3 = 6
+
+->Quinta vez no for
+i = 4
+valor = 6
+valor = valor + i
+valor = 6 + 4 = 10 
+
+O resultado visto no console será:
+10
+
+*/
