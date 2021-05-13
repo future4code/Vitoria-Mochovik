9e57b3f9242503465206a130dec8cc3eb1172d35
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-13 11:43:14 -0300

    Exercício 01 de interpretação de código

---
+++ semana2/funcoes-de-array/index.js
@@ -0,0 +1,23 @@
+//----------------------Exercícios de interpretação de código-------------------------------------
+
+//EXERCÍCIO 01------------------------------------------------------------------------------------
+
+  /*
+  Será impresso no console:
+  { nome: 'Amanda Rangel', apelido: 'Mandi' } 0 [
+  { nome: 'Amanda Rangel', apelido: 'Mandi' },
+  { nome: 'Laís Petra', apelido: 'Laura' },
+  { nome: 'Letícia Chijo', apelido: 'Chijo' }
+]
+{ nome: 'Laís Petra', apelido: 'Laura' } 1 [
+  { nome: 'Amanda Rangel', apelido: 'Mandi' },
+  { nome: 'Laís Petra', apelido: 'Laura' },
+  { nome: 'Letícia Chijo', apelido: 'Chijo' }
+]
+{ nome: 'Letícia Chijo', apelido: 'Chijo' } 2 [
+  { nome: 'Amanda Rangel', apelido: 'Mandi' },
+  { nome: 'Laís Petra', apelido: 'Laura' },
+  { nome: 'Letícia Chijo', apelido: 'Chijo' }
+]
+ 
+  */
