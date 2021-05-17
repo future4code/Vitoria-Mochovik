f715fd9d3af5cbec5e3a11f6d561cea3cadeb087
Vitória Mochovik <mochovik.vc@gmail.com>
2021-05-17 15:09:39 -0300

    Exercício 12

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -146,10 +146,16 @@ function ordenaArray(array) {
    return array
 }
 
-// Exercício 12
+// Exercício 12-----------------------------------------------------------------------------------------
 
 function filmeFavorito() {
-   // implemente sua lógica aqui
+   let filmeFavorito = {
+      nome: `O Diabo Veste Prada`,
+      ano: 2006,
+      diretor: `David Frankel`,
+      atores: [ `Meryl Streep`, `Anne Hathaway`, `Emily Blunt`, `Stanley Tucci`]
+   }
+   return filmeFavorito
 }
 
 // Exercício 13
