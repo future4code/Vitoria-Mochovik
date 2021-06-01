7b0a144934c943d021501cfb83ad3a995ba78b82
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-01 14:36:16 -0300

    Alterei minhas informações pessoais

---
+++ semana5/labedin/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import ImagemPerfil from '/home/vitoria/Documents/Repositorios/Vitoria-Mochovik/semana5/labedin/src/img/WhatsApp Image 2020-09-07 at 10.12.47.jpeg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={ImagemPerfil}
+          nome="Vitória Carolina Mochovik" 
+          descricao="Oi, eu sou a Vitória. Sou estudante de engenharia de computação na UFSC e aluna da Labenu."
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Tratare" 
+          descricao="Auxiliar de escritório" 
+        />
+        
+        {/* <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        /> */}
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
