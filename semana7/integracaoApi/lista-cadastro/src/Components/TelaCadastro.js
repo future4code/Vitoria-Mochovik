4f6643c5bee3e3aa531b8d4426c60b3a94deae83
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-16 18:28:28 -0300

    Adicionei uma estilização a tela de cadastro e adicionei um alert de confirmação antes de deletar um usuario

--- semana7/integracaoApi/lista-cadastro/src/Components/TelaCadastro.js
+++ semana7/integracaoApi/lista-cadastro/src/Components/TelaCadastro.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import BackgroundImage from '../img/back-loguin.png'
+import ImagemIcone from '../img/icone-perfil4.png'
+
 
     const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
     const headers = {
@@ -7,6 +11,81 @@ import axios from 'axios';
             Authorization: "vitoria-mochovik-molina"
         }
     }
+
+const Background = styled.div`
+  background-image: url(${BackgroundImage});
+  margin: 0px;
+  padding: 0px;
+  height: 100vh;
+
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p{
+    margin: 0px;
+  }
+`
+
+const CardCadastro = styled.div`
+  background-color: rgb(255, 255, 255, 0.7);
+  align-items: center;
+  justify-content: center;
+  padding: 35px;
+  border-radius: 15px;
+  margin: 0px 500px 0px 500px; 
+  height: 150px;
+
+ 
+`
+
+const Input = styled.input `
+  width: 300px;
+  margin-top: 25px;
+  height: 25px;
+`
+
+const Button = styled.button`
+
+  height: 35px;
+  width: 100px;
+
+`
+
+const IconePerfil = styled.img`
+  width: 100px;
+  background-color: #340059;
+  border-radius: 150px;
+  margin-bottom: 280px;
+ position: absolute;
+  
+  
+
+`
+
+const ContainerButton = styled.div`
+   
+   display: flex;
+   justify-content:center;
+   align-items: center;
+   margin-top: 20px;
+
+`
+
+const ContainerIcone = styled.div`
+  
+  border-left: 1px solid black;
+  
+`
+
+const ContainerBotaoLista = styled.div`
+  margin-top: 35px;
+`
+
+const ButtonLista = styled.button`
+  width: 15
+  0px;
+`
 
 export default class Cadastro extends React.Component {
 
@@ -48,22 +127,33 @@ export default class Cadastro extends React.Component {
 
     render() {
         return (
-            <div>
-                <p> Nome: </p>
-                <input 
+            <Background>
+              {/* <ContainerIcone> */}
+                <IconePerfil src={ImagemIcone} />
+              {/* </ContainerIcone> */}
+              
+              <CardCadastro>
+                
+                {/* <p> Nome: </p> */}
+                <Input 
                   value={this.state.inputName} 
                   onChange={this.onChangeInputNome} 
                   placeholder={"Nome"}
                 /> 
-                <p> E-mail</p>
-                <input 
+                {/* <p> E-mail</p> */}
+                <Input 
                   value={this.state.inputEmail} 
                   onChange={this.onChangeInputEmail} 
                   placeholder={"E-mail"} 
                 />
-                <button onClick={this.criarUsuario}> Salvar</button>
-                <button onClick={this.props.irParaListaPessoasCadastradas} > Ver lista de usuários</button> 
-            </div>
+                <ContainerButton>
+                  <Button onClick={this.criarUsuario}> Salvar</Button>
+                </ContainerButton>
+              </CardCadastro>
+              <ContainerBotaoLista>
+                <ButtonLista onClick={this.props.irParaListaPessoasCadastradas} > Ver lista de usuários</ButtonLista> 
+              </ContainerBotaoLista>
+            </Background>
         )
     }
 }
--- semana7/integracaoApi/lista-cadastro/src/Components/TelaListaPessoasCadastradas.js
+++ semana7/integracaoApi/lista-cadastro/src/Components/TelaListaPessoasCadastradas.js
@@ -1,7 +1,26 @@
 import axios from 'axios';
 import React from 'react';
+import styled from 'styled-components'
 
 
+const CardNomes = styled.div `
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    display: flex;
+    justify-content: space-between;
+`
+const ContainerButton = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 15px;
+
+`
+
+const Button = styled.button`
+
+`
 export default class TelaListaPessoasCadastradas extends React.Component {
     
     state = {
@@ -33,34 +52,44 @@ export default class TelaListaPessoasCadastradas extends React.Component {
     }
 
     deleteUsuario = (id) => {
-        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
-        const headers = {
-            headers: {
-                Authorization: "vitoria-mochovik-molina"
-            }
-        }
 
-        axios.delete(url, headers)
-        .then((res) => {
-            console.log(res)
-            this.pegarUsuarios()
-        })
-        .catch((err) => {
-            alert(err.response.data)
-        })
+        let deletar = window.confirm("Tem certeza de que deseja deletar?") 
+
+        if (deletar === true) {
+            const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+            const headers = {
+                headers: {
+                    Authorization: "vitoria-mochovik-molina"
+                }
+            }
+
+            axios.delete(url, headers)
+            .then((res) => {
+                console.log(res)
+                this.pegarUsuarios()
+            })
+            .catch((err) => {
+                alert(err.response.data)
+            })
+        }
+      
     }
-    
+        
     render() {
 
         const listaUsuarios = this.state.usuarios.map((usuario) => {
-            return <div key={usuario.id}> 
+            return <CardNomes key={usuario.id}> 
                 {usuario.name} 
                 <button onClick={() => this.deleteUsuario(usuario.id)}> X </button>
-            </div>
+            </CardNomes>
         })
         return (
             <div>
-                <button onClick={this.props.irParaCadastro}> Ir para cadastro </button>
+                <ContainerButton>
+                    <Button onClick={this.props.irParaCadastro}> Ir para cadastro </Button>
+                    
+                </ContainerButton>
+                <h2> Lista de Usuários Cadastrados </h2>
                 {listaUsuarios}
             </div>
         )
