18fea814d38651b15a6656308ccfe4815305c7ea
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-18 14:46:59 -0300

    Criei campo de input para o nome da playlist além de desenvolver a parte da requisição ao criar a playlist

--- semana7/projeto-labefy/labefy/src/Components/TelaCriarPlaylist.js
+++ semana7/projeto-labefy/labefy/src/Components/TelaCriarPlaylist.js
@@ -2,10 +2,46 @@ import axios from 'axios';
 import React from 'react';
 
 export default class TelaVisualizarPlaylist extends React.Component {
+
+    state = {
+        namePlaylist: "",
+    }
+
+    createPlaylist = () => {
+        const url= `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists`
+        const headers = {
+            headers: {
+                Authorization: "vitoria-mochovik-molina"
+            }
+        }
+
+        const body = {
+            name: this.state.namePlaylist
+        }
+
+        axios.post(url, body, headers) 
+        .then((res) => {
+            alert("Playlist cadastrada com sucesso!")
+            this.setState({ namePlaylist: ""})
+        })
+        .catch((err) => {
+            alert(err.response.data.message)
+        })
+    }
+
+    onChangeNamePlaylist = (event) => {
+        this.setState({ namePlaylist: event.target.value})
+    }
+
     render() {
         return(
             <div>
-                estou viasualiando playlist
+                <input 
+                    placeholder={"Nome da playlist"}
+                    onChange={this.onChangeNamePlaylist}
+                    value={this.state.namePlaylist}
+                />
+                <button onClick={this.createPlaylist}> Cadastrar</button>
             </div>
         )
     }
