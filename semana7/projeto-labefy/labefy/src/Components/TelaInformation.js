9d02950f21673728d60c72f8ed9e7a8c5a7da519
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-18 21:42:09 -0300

    Adicionei os controles junto as musicas

--- semana7/projeto-labefy/labefy/src/Components/TelaInformation.js
+++ semana7/projeto-labefy/labefy/src/Components/TelaInformation.js
@@ -6,7 +6,12 @@ import TelaVisualizarPlaylist from './TelaVisualizarPlaylist.js'
 export default class TelaInformation extends React.Component {
 
     state = {
-        musicas: []
+        musicas: [],
+        adicionarMusic: false,
+        inputArtista: "",
+        inputName: "",
+        inputUrl: ""
+
     }
    
     getMusicasPlaylist =() => {
@@ -20,6 +25,7 @@ export default class TelaInformation extends React.Component {
 
         axios.get(url, headers)
         .then((res) => {
+            this.setState({ musicas: res.data.result.tracks})
             console.log("informacoes user res", res.data.result.tracks)
         })
         .catch((err) => {
@@ -28,20 +34,96 @@ export default class TelaInformation extends React.Component {
     
     }
 
+    onChangeadicionarMusic =() => {
+        this.setState({adicionarMusic: true})
+    }
+
+    onChangeInputName = (event) => {
+        this.setState({ inputName: event.target.value})
+    }
+
+    onChangeInputArtista = (event) => {
+        this.setState({ inputArtista: event.target.value})
+    }
+
+    onChangeUrl = (event) => {
+        this.setState({inputUrl: event.target.value })
+    }
+
+    AdicionarMucisPlaylist = () => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.id}/tracks`
+        const headers = {
+            headers: {
+                Authorization: "vitoria-mochovik-molina"
+            }
+        }
+
+        const body = {
+            name: this.state.inputName,
+            artist: this.state.inputArtista,
+            url: this.state.inputUrl
+        }
+
+        axios.post(url, body, headers)
+        .then((res) => {
+            this.getMusicasPlaylist()
+            this.setState({
+                inputName: "",
+                inputArtista: "",
+                inputUrl: "",
+                adicionarMusic: false,
+
+            })
+        })
+        .catch((err) => {
+            alert("nao deu")
+        })
+    }
+
 
     render() {
 
         const musicList = this.state.musicas.map((musica) => {
             return(
-                <p> {musica.name}</p>
+                <div>
+                    <p> {musica.name}</p>
+                    <audio controls="controls">
+                        <source src={musica.url} type="audio/ogg" />
+                    </audio> 
+                </div>
             )
         })
+        console.log("music list", musicList)
 
         return (
             <div>
                 {this.getMusicasPlaylist()}
                 {this.props.playlistName}
                 {musicList}
+
+
+                {this.state.adicionarMusic === true ? 
+                    <div>
+                        <input 
+                            value={this.state.inputName}   
+                            onChange={this.onChangeInputName}
+                            placeholder={"nome da musica"}
+                        />
+                        <input
+                            value={this.state.inputArtista}
+                            onChange={this.onChangeInputArtista}
+                            placeholder={"artista"}
+                        />
+                        <input
+                            value={this.state.inputUrl}
+                            onChange={this.onChangeUrl}
+                            placeholder={"url da musica"}
+                        />
+                        <button onClick={this.AdicionarMucisPlaylist}> Adicionar </button>
+                    </div>
+            
+                
+                : <button onClick={this.onChangeadicionarMusic}> Adicionar música</button> }
             </div>
         )
     }
