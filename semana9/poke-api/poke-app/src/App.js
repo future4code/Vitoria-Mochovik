9cf8a08404574d064d989d613dcb93214788609c
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-29 21:44:41 -0300

    :arrow_up: Mudei os states pata useState, mudei didMount para useEffect, ou seja, usei hooks para tais funcionalidades

--- semana9/poke-api/poke-app/src/App.js
+++ semana9/poke-api/poke-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import PokeCard from './components/PokeCard/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [pokeList, setPokeList] = useState([])
+    const [pokeName, setPokeName] = useState("")
+
+
+    useEffect(() => {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+        .then(response => {
+          setPokeList(response.data.results)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }, [pokeName])
+  
+
+  const changePokeName = event => {
+    setPokeName(event.target.value)
+  }
+
+  
+    return(
+      <div className="App">
+        <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            )
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName}/>}
+      </div>
+    )
+  
 }
 
 export default App;
