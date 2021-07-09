43664093c8a14dea17e65a872fa903ff34534274
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-07-09 20:16:32 -0300

    :package: Página de criar viagem feita

--- semana10/projeto-labeX/labex/src/pages/createTrip/CreateTripPage.js
+++ semana10/projeto-labeX/labex/src/pages/createTrip/CreateTripPage.js
@@ -1,9 +1,98 @@
+import axios from 'axios'
 import React from  'react'
+import useForm from '../hooks/useForm'
+import {useHistory} from "react-router-dom"
+
 
 const CreateTripPage = () => {
+    const { form, onChange, cleanFields } = useForm({
+        name: "",
+        planet: "",
+        date: "",
+        description: "",
+        durationInDays: "",
+        
+
+    })
+    const history = useHistory()
+    const token = window.localStorage.getItem("token")
+
+    const create = (event) =>{
+        event.preventDefault()
+
+        const url =`https://us-central1-labenu-apis.cloudfunctions.net/labeX/vitoria-mochovik-molina/trips`
+        const headers = {
+            auth: token
+        }
+
+        axios.post(url, form, {headers})
+        .then((res) => {
+            console.log("deu certo viagem criada", res.data)
+            cleanFields()
+        })
+        .catch((err) => {
+            console.log("deu erro para criar ", err.message)
+        })
+
+
+    }
+
+    const goBack = () => {
+        history.goBack()
+    }
     return(
         <div>
             <p> CreateTripPage</p>
+            <form onSubmit={create}>
+                <input 
+                    name="name"
+                    type="name"
+                    placeholder={"Nome"} 
+                    value={form.name} 
+                    onChange={onChange}
+                    required
+                />
+                <select name="planet" onChange={onChange} required>
+                    <option value="Escolha um planeta"> Escolha um planeta</option>
+                    <option value="Mercurio"> Mercurio </option>
+                    <option value="venus"> Venus </option>
+                    <option value="terra"> Terra </option>
+                    <option value="marte"> Marte </option>
+                    <option value="jupiter"> Jupiter </option>
+                    <option value="saturno"> Saturno </option>
+                    <option value="urano"> Urano </option>
+                    <option value="netuno"> Netuno </option>
+                    <option value="plutao"> Plutão </option>
+                    
+                </select>
+                <input 
+                    name="date"
+                    type="date"
+                    value={form.date} 
+                    onChange={onChange}
+                    required
+                />
+                <input 
+                    name="description"
+                    type="text"
+                    placeholder={"Descrição"} 
+                    value={form.description} 
+                    onChange={onChange}
+                    required
+                />
+                 <input 
+                    name="durationInDays"
+                    type="number"
+                    placeholder={"Duração em dias"} 
+                    value={form.durationInDays} 
+                    onChange={onChange}
+                    required
+                    
+                />
+                
+                <button> Criar </button>
+            </form>
+            <button onClick={goBack}> Voltar </button>
         </div>
     )
 }
