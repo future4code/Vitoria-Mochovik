5abbfc1208db23a61169e52238d70c294bd90d76
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-07-11 21:24:47 -0300

    :arrow_up: Estilizei a página de criar viagem

--- semana10/projeto-labeX/labex/src/pages/createTrip/CreateTripPage.js
+++ semana10/projeto-labeX/labex/src/pages/createTrip/CreateTripPage.js
@@ -3,7 +3,8 @@ import React from  'react'
 import useForm from '../hooks/useForm'
 import {useHistory} from "react-router-dom"
 
-
+import { ContainerForm, Input, Button, Select, CardForm,
+    ContainerTitulo, ContainerGeral, ContainerButton, ButtonDestaque} from './styled'
 const CreateTripPage = () => {
     const { form, onChange, cleanFields } = useForm({
         name: "",
@@ -41,59 +42,74 @@ const CreateTripPage = () => {
         history.goBack()
     }
     return(
-        <div>
-            <p> CreateTripPage</p>
-            <form onSubmit={create}>
-                <input 
-                    name="name"
-                    type="name"
-                    placeholder={"Nome"} 
-                    value={form.name} 
-                    onChange={onChange}
-                    required
-                />
-                <select name="planet" onChange={onChange} required>
-                    <option value="Escolha um planeta"> Escolha um planeta</option>
-                    <option value="Mercurio"> Mercurio </option>
-                    <option value="Venus"> Venus </option>
-                    <option value="Terra"> Terra </option>
-                    <option value="Marte"> Marte </option>
-                    <option value="Jupiter"> Jupiter </option>
-                    <option value="Saturno"> Saturno </option>
-                    <option value="Urano"> Urano </option>
-                    <option value="Netuno"> Netuno </option>
-                    <option value="Plutao"> Plutão </option>
+        <ContainerGeral>
+            <ContainerTitulo>
+                <h2> Criar Viagem </h2>
+            </ContainerTitulo>
+            
+            
+                <form onSubmit={create}>
+                <ContainerForm>
+                    <CardForm>
+
                     
-                </select>
-                <input 
-                    name="date"
-                    type="date"
-                    value={form.date} 
-                    onChange={onChange}
-                    required
-                />
-                <input 
-                    name="description"
-                    type="text"
-                    placeholder={"Descrição"} 
-                    value={form.description} 
-                    onChange={onChange}
-                    required
-                />
-                 <input 
-                    name="durationInDays"
-                    type="number"
-                    placeholder={"Duração em dias"} 
-                    value={form.durationInDays} 
-                    onChange={onChange}
-                    required
+                    <Input 
+                        name="name"
+                        type="name"
+                        placeholder={"Nome"} 
+                        value={form.name} 
+                        onChange={onChange}
+                        required
+                    />
+                    <Select name="planet" onChange={onChange} required>
+                        <option value="Escolha um planeta"> Escolha um planeta</option>
+                        <option value="Mercurio"> Mercurio </option>
+                        <option value="Venus"> Venus </option>
+                        <option value="Terra"> Terra </option>
+                        <option value="Marte"> Marte </option>
+                        <option value="Jupiter"> Jupiter </option>
+                        <option value="Saturno"> Saturno </option>
+                        <option value="Urano"> Urano </option>
+                        <option value="Netuno"> Netuno </option>
+                        <option value="Plutao"> Plutão </option>
+                        
+                    </Select>
+                    <Input 
+                        name="date"
+                        type="date"
+                        value={form.date} 
+                        onChange={onChange}
+                        required
+                    />
+                    <Input 
+                        name="description"
+                        type="text"
+                        placeholder={"Descrição"} 
+                        value={form.description} 
+                        onChange={onChange}
+                        required
+                    />
+                    <Input 
+                        name="durationInDays"
+                        type="number"
+                        placeholder={"Duração em dias"} 
+                        value={form.durationInDays} 
+                        onChange={onChange}
+                        required
+                        
+                    />
+                    <ContainerButton>
+                        <ButtonDestaque> CRIAR </ButtonDestaque> 
+                    </ContainerButton>
                     
-                />
-                
-                <button> Criar </button>
-            </form>
-            <button onClick={goBack}> Voltar </button>
-        </div>
+                    </CardForm>
+                </ContainerForm>
+                </form>
+                <ContainerButton>
+                    <Button onClick={goBack}> VOLTAR </Button>
+                </ContainerButton>
+            
+        </ContainerGeral>
     )
 }
 export default CreateTripPage
