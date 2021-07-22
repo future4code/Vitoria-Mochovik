5b5817fb72adff6a04c35c9b27eda457b05b99a3
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-07-22 16:05:04 -0300

    :package: Requisições dos posts do feed

---
+++ semana12/projetoLabeddit/labeddit/src/hooks/useRequestData.js
@@ -0,0 +1,23 @@
+import { useEffect, useState} from 'react'
+import axios from "axios"
+
+const useRequestData = (initialData, url) => {
+    const [data, setData] = useState(initialData)
+    const token = localStorage.getItem("token")
+    useEffect(() => {
+        axios.get(url, {
+            headers: {
+                Authorization: token
+            }
+        })
+        .then((res) => {
+            setData(res.data)
+        })
+        .catch((err) => {
+            alert("Ocorreu um erro, tente novamente")
+        })
+    }, [url])
+    return (data)
+}
+
+export default useRequestData
--- semana12/projetoLabeddit/labeddit/src/pages/FeedPage/FeedPage.js
+++ semana12/projetoLabeddit/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { BASE_URL } from '../../constants/urls'
 import useProtectedPage from '../../hooks/useProtectedPage'
+import useRequestData from '../../hooks/useRequestData'
 
 const FeedPage = () => {
     useProtectedPage()
+    const posts = useRequestData([], `${BASE_URL}/posts`)
+    console.log("posts", posts)
+
+    const postCard = posts.map((post) => {
+        return(
+            <div key={post.id}>
+                <h4> {post.username} </h4>
+                <p> {post.title}</p>
+            </div>
+        )
+    })
     return(
         <div>
             FeedPage
+            <div>
+                {postCard}
+            </div>
         </div>
     )
 }
