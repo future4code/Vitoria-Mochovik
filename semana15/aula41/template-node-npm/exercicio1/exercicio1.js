d89ecee7fbd42dc0b0042b1381341bac90970b1a
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-10 15:44:35 -0300

    :package: Exercicio 01

---
+++ semana15/aula41/template-node-npm/exercicio1/exercicio1.js
@@ -0,0 +1,18 @@
+//QUESTÃO 01 
+
+/*
+
+A-  
+    process.argv[2]
+    process.argv[3]
+    ...
+    sempre a partir da posição 2 do array
+
+*/
+
+//B-
+
+const name = process.argv[2]
+const age = Number(process.argv[3])
+
+console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${age + 7}`)
