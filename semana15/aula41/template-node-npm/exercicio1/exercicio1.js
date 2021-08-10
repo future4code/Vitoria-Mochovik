9df922c0b067f2290f26c3e222e4f6920e18be61
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-10 17:33:04 -0300

    :package: Desafio 01

--- semana15/aula41/template-node-npm/exercicio1/exercicio1.js
+++ semana15/aula41/template-node-npm/exercicio1/exercicio1.js
@@ -15,4 +15,10 @@ A-
 const name = process.argv[2]
 const age = Number(process.argv[3])
 
-console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${age + 7}`)
+if (name && age ) {
+  console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${age + 7}`)  
+} else if ( !name  && !age ) {
+    console.log("Por favor, digite primeiro seu nome depois a idade")
+} else if((!name && age) || (!age && name)) {
+    console.log("Preciso de dois parametro! Por favor, digite seu nome e em seguida sua idade")
+} 
--- semana15/aula41/template-node-npm/exercicio2/exercicio2.js
+++ semana15/aula41/template-node-npm/exercicio2/exercicio2.js
@@ -5,21 +5,34 @@ const number1 = Number(process.argv[3])
 const number2 = Number(process.argv[4])
 let result
 
-switch(operation){
-    case 'add':
-        result = number1 + number2
-        break;
-    case 'sub':
-        result = number1 - number2
-        break;
-    case 'mult':
-        result = number1 * number2
-        break;
-    case 'div':
-        result = number1 / number2
-        break;
-    default:
-        result = "Valores ou operação inválida. Digite a operação (add, sub, mult ou div) e dois números, exemplo: 'add 2 2'"
+if(operation && number1 && number2){
+    switch(operation){
+        case 'add':
+            result = number1 + number2
+            break;
+        case 'sub':
+            result = number1 - number2
+            break;
+        case 'mult':
+            result = number1 * number2
+            break;
+        case 'div':
+            result = number1 / number2
+            break;
+        default:
+            result = "Valores ou operação inválida. Digite a operação (add, sub, mult ou div) e dois números, exemplo: 'add 2 2'"
+    }  
+} else if((!operation && number1 && number2) ||
+    (operation && !number1 && number2) || 
+    (operation && number1 && !number2)){
+        result = "Você inseriu apenas dois parametro, insira 3. Exemplo: add 2 2 "
+} else if((!operation && !number1 && number2) ||
+    (!operation && number1 && !number2) || 
+    (operation && !number1 && !number2)){
+        result = "Você inseriu apenas um parametro, insira 3. Exemplo: add 2 2 "
+} else if(!operation && !number1 && !number2) {
+    result = "Você não inseriu nenhum parametro, insira 3. Exemplo: add 2 2 "
 }
+
 
 console.log(`Resultado: ${result}`)
