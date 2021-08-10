6e4d36e7be453309f0b0f4ba911a17f940107810
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-10 16:17:24 -0300

    :package: Exercicio 04

--- semana15/aula41/template-node-npm/exercicio3/exercicio3.js
+++ semana15/aula41/template-node-npm/exercicio3/exercicio3.js
@@ -4,5 +4,6 @@ let toDoList = ["Assistir aula", "Fazer um bom projeto"]
 const assignment = process.argv[2]
 toDoList.push(assignment)
 
+console.log("Tarefa adicionada com sucesso!")
 console.table(toDoList)
 console.log('Se sua atividade apareceu separada, escreva entre aspas, assim: "varrer a casa" ')
