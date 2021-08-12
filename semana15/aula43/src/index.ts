254a732d9422b050e965235c2d45a1c7c1770064
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-12 19:34:33 -0300

    :package: Exercicio 02

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -7,6 +7,12 @@ const app: Express = express()
 app.use(express.json())
 app.use(cors())
 
+app.listen(3003, () => {
+    console.log("Servidor pronto!")
+})
+
+//EXERCICIO 01
+
 app.get('/countries', (req: Request, res: Response) => {
     const result = countries.map((country: country) => {
         return { id: country.id, name: country.name}
@@ -14,6 +20,22 @@ app.get('/countries', (req: Request, res: Response) => {
     res.status(200).send(result)
 })
 
-app.listen(3003, () => {
-    console.log("Servidor pronto!")
- })
+//EXERCICIO 02
+
+app.get('/countries/:id', (req: Request, res: Response) => {
+    let errorCode: number = 400
+    try {
+        const result: country | undefined = countries.find(
+            country => country.id ===Number(req.params.id)
+        )  
+        if(!result) {
+            errorCode = 404;
+            throw new Error();
+        }
+        res.status(200).send(result)
+    } catch (error) {
+        console.log(error)
+        res.status(errorCode).send(error.message)
+    }
+    
+})
