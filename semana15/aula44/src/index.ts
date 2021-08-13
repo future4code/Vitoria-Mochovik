8e8b66384058b972f649e5f228afa7e015e2e5b0
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-13 11:55:34 -0300

    :package: Exercicio 02

--- semana15/aula44/src/index.ts
+++ semana15/aula44/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express} from "express"
+import express, {Express, Request, Response} from "express"
 import cors from 'cors'
 import { AddressInfo } from "net"
 
@@ -13,4 +13,83 @@ const server = app.listen(process.env.PORT || 3003, () => {
     } else {
         console.error(`Failure upon starting server.`)
     }
+})
+
+enum USER_ROLES {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    type: USER_ROLES,
+    age: number
+}
+
+let users: User[] = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: USER_ROLES.NORMAL,
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: USER_ROLES.ADMIN,
+        age: 60
+    }
+]
+
+
+app.get("/users", (req: Request, res: Response) => {
+    let type = req.query.type as string
+
+    try {
+        if(type) {
+            type = type.toUpperCase()
+
+            if(type in USER_ROLES){
+                const result = users.filter(user => user.type === type)
+                return res.status(200).send(result)
+            }
+
+            throw new Error("Invalid type")
+        }   
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+    
 })
