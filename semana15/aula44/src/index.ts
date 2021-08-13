3802b43d4af6551c4c17a15ae8a731aa4fa13907
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-13 14:52:09 -0300

    :package: Exercicio 04

--- semana15/aula44/src/index.ts
+++ semana15/aula44/src/index.ts
@@ -110,3 +110,57 @@ app.get("/users", (req: Request, res: Response) => {
     
 })
 
+app.post("/users", (req: Request, res: Response) => {
+    try {
+        const { name, email, age} = req.body
+        let type = req.body.type as string 
+
+        if(!name || !email || !age || !type){
+            throw new Error("Missing data in body to create the user")
+        }
+
+        if(typeof name !== "string"){
+            throw new Error("Invalid name")
+        }
+
+        if(typeof email !== "string") {
+            throw new Error("Invalid email");
+        }
+
+        if(typeof age !== "number") {
+            throw new Error("Invalid age");
+        }
+
+        type = type.toUpperCase()
+        if (!(type in USER_ROLES)){
+            throw new Error("Invalid type");
+        }
+
+        users.forEach(user => {
+            if (user.email === email) {
+                throw new Error("Email already exists"); 
+            }
+        })
+
+        const newUser: User = {
+            id: users.length+1,
+            name,
+            email,
+            age,
+            type: type === USER_ROLES.NORMAL
+                ? USER_ROLES.NORMAL
+                : USER_ROLES.ADMIN
+        }
+
+        users.push(newUser)
+
+        res.status(200).send({
+            message: "User created successfuly",
+            user: newUser
+        })
+
+
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
