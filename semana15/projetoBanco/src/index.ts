95086f65c4f3af7ba9a8eac8880a1608ea4be8ec
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-08-17 15:45:11 -0300

    :arrow_up: Agrupamento de endpoints do mesmo método

--- semana15/projetoBanco/src/index.ts
+++ semana15/projetoBanco/src/index.ts
@@ -46,6 +46,125 @@ app.post("/users/create", (req: Request, res: Response) => {
     }
 })
 
+//TRANSFERÊNCIA INTERNA
+
+app.post("/users/transfer", (req: Request, res: Response) => {
+    try{
+        const {recipient, sender, value} = req.body
+
+        if(!recipient.name || !recipient.cpf || !sender.name || !sender.cpf || !value) {
+            throw new Error("Inform: sender{name, cpf}, recipient{name, cpf}, value");
+        }
+
+        if(typeof value !== "number") {
+            throw new Error("Please enter a valid value for transfer");
+        }
+
+        if(value < 0){
+            throw new Error("Enter a value greater than 0");
+        }
+
+        let clients = openFile()
+        const clientSenderIndex = clients.findIndex(person => person.cpf === sender.cpf && person.name === sender.name)
+        const clientRecipientIndex = clients.findIndex(person => person.cpf === recipient.cpf && person.name === recipient.name)
+
+        if(clientSenderIndex <0 || clientRecipientIndex<0){
+            if(clientSenderIndex<0 && clientRecipientIndex<0) {
+                throw new Error("Sending customer and receiving customer not found");
+            }
+
+            if(clientSenderIndex<0){
+                throw new Error("Sending customer not found");
+            }
+
+            if(clientRecipientIndex <0) {
+                throw new Error("Recipient customer not found");
+            }
+        }
+
+        if(clients[clientSenderIndex].balance < value) {
+            throw new Error("Insufficient funds");
+        }
+
+        const send : Statement = {
+            refresh: true,
+            value: value*-1,
+            description: "Transferência bancaria para" + recipient.name,
+            date: new Date()
+        }
+
+        const recip : Statement = {
+            refresh: true,
+            value: value,
+            description: "Transferência recebida de" + sender.name,
+            date: new Date()
+        }
+
+        
+        clients[clientSenderIndex].statement.push(send)
+        clients[clientRecipientIndex].statement.push(recip)
+        saveFile(clients)
+
+        res.status(200).send("Transfer successful!")
+
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+//PAGAR CONTA
+
+app.post("/users/pay", (req: Request, res: Response) => {
+    try {
+        let {cpf, description, value, date} = req.body
+
+        if(!cpf || !description || !value ) {
+            throw new Error("Inform cpf, description, value and date")
+        }
+
+        if(typeof value !== "number") {
+            throw new Error("Enter a value in number format")
+        }
+
+        if( value <= 0) {
+            throw new Error("Enter a value greater than 0");
+        }
+
+        if(!date){
+            date = new Date()
+        }
+
+        if(!analizeTodayDate(date)){
+            throw new Error("Date entered must be greater than or equal to current date");
+        }
+
+        const clients = openFile()
+        const clientIndex = clients.findIndex(person => person.cpf === cpf)
+
+        if(clientIndex<0){
+            throw new Error("Customer not found"); 
+        }
+
+        if(value > clients[clientIndex].balance){
+            throw new Error("Insufficient funds");
+        }
+
+        const statement: Statement={
+            value: value*-1,
+            date,
+            description,
+            refresh: true
+        }
+
+        clients[clientIndex].statement.push(statement)
+        saveFile(clients)
+
+        res.status(200).send(statement)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
 //PEGAR TODOS OS CLIENTES
 
 app.get("/users/all", (req: Request, res: Response) => {
@@ -124,61 +243,6 @@ app.put("/users/add", (req: Request, res: Response) => {
     }
 })
 
-
-//PAGAR CONTA
-
-app.post("/users/pay", (req: Request, res: Response) => {
-    try {
-        let {cpf, description, value, date} = req.body
-
-        if(!cpf || !description || !value ) {
-            throw new Error("Inform cpf, description, value and date")
-        }
-
-        if(typeof value !== "number") {
-            throw new Error("Enter a value in number format")
-        }
-
-        if( value <= 0) {
-            throw new Error("Enter a value greater than 0");
-        }
-
-        if(!date){
-            date = new Date()
-        }
-
-        if(!analizeTodayDate(date)){
-            throw new Error("Date entered must be greater than or equal to current date");
-        }
-
-        const clients = openFile()
-        const clientIndex = clients.findIndex(person => person.cpf === cpf)
-
-        if(clientIndex<0){
-            throw new Error("Customer not found"); 
-        }
-
-        if(value > clients[clientIndex].balance){
-            throw new Error("Insufficient funds");
-        }
-
-        const statement: Statement={
-            value: value*-1,
-            date,
-            description,
-            refresh: true
-        }
-
-        clients[clientIndex].statement.push(statement)
-        saveFile(clients)
-
-        res.status(200).send(statement)
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-})
-
-
 // ATUALIZAR SALDO
 
 app.put("/users/refresh?", (req: Request, res: Response) => {
@@ -205,73 +269,6 @@ app.put("/users/refresh?", (req: Request, res: Response) => {
     }
 
 })
-
-//TRANSFERÊNCIA INTERNA
-
-app.post("/users/transfer", (req: Request, res: Response) => {
-    try{
-        const {recipient, sender, value} = req.body
-
-        if(!recipient.name || !recipient.cpf || !sender.name || !sender.cpf || !value) {
-            throw new Error("Inform: sender{name, cpf}, recipient{name, cpf}, value");
-        }
-
-        if(typeof value !== "number") {
-            throw new Error("Please enter a valid value for transfer");
-        }
-
-        if(value < 0){
-            throw new Error("Enter a value greater than 0");
-        }
-
-        let clients = openFile()
-        const clientSenderIndex = clients.findIndex(person => person.cpf === sender.cpf && person.name === sender.name)
-        const clientRecipientIndex = clients.findIndex(person => person.cpf === recipient.cpf && person.name === recipient.name)
-
-        if(clientSenderIndex <0 || clientRecipientIndex<0){
-            if(clientSenderIndex<0 && clientRecipientIndex<0) {
-                throw new Error("Sending customer and receiving customer not found");
-            }
-
-            if(clientSenderIndex<0){
-                throw new Error("Sending customer not found");
-            }
-
-            if(clientRecipientIndex <0) {
-                throw new Error("Recipient customer not found");
-            }
-        }
-
-        if(clients[clientSenderIndex].balance < value) {
-            throw new Error("Insufficient funds");
-        }
-
-        const send : Statement = {
-            refresh: true,
-            value: value*-1,
-            description: "Transferência bancaria para" + recipient.name,
-            date: new Date()
-        }
-
-        const recip : Statement = {
-            refresh: true,
-            value: value,
-            description: "Transferência recebida de" + sender.name,
-            date: new Date()
-        }
-
-        
-        clients[clientSenderIndex].statement.push(send)
-        clients[clientRecipientIndex].statement.push(recip)
-        saveFile(clients)
-
-        res.status(200).send("Transfer successful!")
-
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-})
-
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
