8289e505cd8d85d5382eeb588b42e7fe29b64b91
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-01 18:51:41 -0300

    :package: Exercicio Desafio - enviar email

--- semana17/aula51/src/endpoints/createUser.ts
+++ semana17/aula51/src/endpoints/createUser.ts
@@ -1,6 +1,11 @@
+import { config } from "dotenv";
 import { Request, Response } from "express";
 import { connection } from "../data/connection";
+import transporter from "../services/mailTransporter";
 import { addressInfo, user, userAddressInfo } from "../types";
+
+
+config()
 
 export default async function createUser(
    req: Request,
@@ -33,9 +38,22 @@ export default async function createUser(
         user_id: id
       }
 
-      
+
       await connection('aula51_users').insert(newUser)
       await connection('userAddress').insert(newAddressInfo)
+
+      const mailInfo = await transporter.sendMail({
+          from: `<${process.env.NODEMAILER_USER}>`,
+          to: `lbn_report_molina-aaaadvqgvvkxbtcijshbktp4ki@labenualunos.slack.com `,
+          subject: `Finalize seu cadastro ${name}`,
+          html: `
+            <p> Bem-vindo ${name}!!! </p>
+            <button> Clique aqui para finalizar seu cadastro </button>
+          `,
+          text: `Utilizei apenas uma tag p para dar Boas-vindas ao usuário cadastrado e 
+          um button para que ele finalize nosso cadastro (sem funcionalidade) `
+      })
+
 
       res.status(201).send("Usuário criado!")
 
