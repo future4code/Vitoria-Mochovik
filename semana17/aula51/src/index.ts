7f90c54b6036ee78a9775d283559f456892a7705
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-01 10:29:06 -0300

    :package: estrutura inicial do template criado

--- semana17/aula51/src/index.ts
+++ semana17/aula51/src/index.ts
@@ -1,20 +1,4 @@
-import express, {Express} from 'express'
-import cors from 'cors'
-import knex from "knex";
-const app: Express = express();
+import app from "./app"
+import createUser from './endpoints/createUser'
 
-app.use(express.json());
-app.use(cors());
-import dotenv from "dotenv";
-
-dotenv.config();
-import { AddressInfo } from "net";
-
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
+app.post('/users/signup', createUser)
