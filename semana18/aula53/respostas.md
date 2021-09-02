22626b0ce4e53b63dcc889e7aa734a3f6250f9ad
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-02 14:52:11 -0300

    :arrow_up: Exercício 01

---
+++ semana18/aula53/respostas.md
@@ -0,0 +1,11 @@
+# Exercícios Aula 53  :memo:  
+
+### Exercício 01
+
+**A-** 
+
+Não seria possível, visto que ela é do tipo privado e não possui nenhum método para acessá-la.
+
+**B-**
+
+Apenas 1 vez, visto que o construtor foi chamado apenas uma vez.
--- semana18/aula53/src/index.ts
+++ semana18/aula53/src/index.ts
@@ -1,14 +1,18 @@
 import express, {Express} from 'express'
 import cors from 'cors'
 import knex from "knex";
-const app: Express = express();
+import { AddressInfo } from "net";
 import dotenv from "dotenv";
+import { User } from './entities/User';
 
+const app: Express = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-import { AddressInfo } from "net";
+//EXERCICIO 01
+const user = new User("1", "vitoria@gmail.com", "Vitoria", "123456")
+console.table({id: user.getId(), name: user.getName(), email: user.getEmail()})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
