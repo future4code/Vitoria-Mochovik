28048d47f4e75b7a1de39729c54daa17f776eaab
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-02 09:34:04 -0300

    :package: Exercicio 02

--- semana18/aula52/src/entities/UserAccount.ts
+++ semana18/aula52/src/entities/UserAccount.ts
@@ -1,9 +1,11 @@
+import { Transaction } from "./Transaction";
+
 export class UserAccount {
     private cpf: string;
     private name: string;
     private age: number;
     private balance: number = 0;
-    // private transactions: Transaction[] = [];
+    private transactions: Transaction[] = [];
   
     constructor(
        cpf: string,
@@ -15,5 +17,46 @@ export class UserAccount {
        this.name = name;
        this.age = age;
     }
-  
+    
+
+    getCpf(){
+        return this.cpf
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getAge(){
+        return this.age
+    }
+
+    getBalance(){
+        return this.balance
+    }
+
+    getTransactions(): Transaction[]{
+        return this.transactions
+    }
+
+
+    setCpf(newCpf: string){
+        this.cpf = newCpf
+    }
+
+    setName(newName: string){
+        this.name = newName
+    }
+
+    setAge(newAge: number){
+        this.age = newAge
+    }
+
+    setBalance(newBalance: number){
+        this.balance = newBalance
+    }
+
+    setTransactions(newTransactions: Transaction[]){
+        this.transactions = newTransactions
+    }
   }
