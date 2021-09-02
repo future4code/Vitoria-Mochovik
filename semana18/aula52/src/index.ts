0f68c7807c97819f1a47a6497b4fe3d4d7261965
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-02 09:19:46 -0300

    :package: Exercicio 01

--- semana18/aula52/src/index.ts
+++ semana18/aula52/src/index.ts
@@ -2,6 +2,7 @@ import express, {Express} from 'express'
 import cors from 'cors'
 import knex from "knex";
 const app: Express = express();
+import { UserAccount} from './entities/UserAccount'
 
 app.use(express.json());
 app.use(cors());
@@ -18,3 +19,26 @@ const server = app.listen(process.env.PORT || 3003, () => {
        console.error(`Failure upon starting server.`);
     }
 });
+
+
+//EXERCICIO 01
+
+/*
+
+1-A
+   O construtor dentro de uma classe serve para inicializar campos que estão internamente na classe,
+   além de alocar recursos que u7m objeto de classe possa precisar.
+   Usamos o 'new nomeDoConstrutor'
+
+1-B
+   Foi chamado uma vez apenas.
+*/
+
+const newUser = new UserAccount("222.111.000-78", "Vitória", 21)
+
+/*
+1-C
+   Fora do construtor por meio das propriedades 'get' e 'set', para visualizar e para atualizar, respectivamente.
+   Ou dentro do construtor usando o 'this' na frente do nome.
+
+*/
