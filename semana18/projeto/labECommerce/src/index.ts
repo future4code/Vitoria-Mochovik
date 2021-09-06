90e3f03df70164da65a6f12813b3891c74a3dd6e
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-06 09:45:07 -0300

    :arrow_up: endpoint de criar viagem

--- semana18/projeto/labECommerce/src/index.ts
+++ semana18/projeto/labECommerce/src/index.ts
@@ -13,13 +13,14 @@ import { createUser } from './endpoints/user/createUser';
 import { createProduct } from './endpoints/product/createProduct';
 import { getAllUsers } from './endpoints/user/getAllUsers';
 import { getAllProducts } from './endpoints/product/getAllProducts';
+import { createTicket } from './endpoints/product/createTicket';
 
 
 app.post("/create/user", createUser)
 app.post("/create/product", createProduct)
+app.post("/create/ticket", createTicket)
 
 app.get("/products", getAllProducts)
-
 app.get("/users", getAllUsers)
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
